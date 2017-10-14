5ced133c71d83bee916c1320a1553288ded08df7
patkorek <patkorek@gmail.com>
2017-10-14 19:08:06 +0200

    Clean project

---
+++ src/main/java/com/WSBGroupProject/WsbGroupProjectApplication.java
@@ -0,0 +1,12 @@
+package com.WSBGroupProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsbGroupProjectApplication.class, args);
+	}
+}
